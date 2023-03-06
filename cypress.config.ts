21250941aa3148aed43093484a550b7373ac1289
ErlinaHima <e.hima2@gmail.com>
2023-03-06 18:29:03 +0100

    update to run test example

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   e2e: {
     specPattern: "cypress/e2e/**/*.{cy,spec}.{js,jsx,ts,tsx}",
-    baseUrl: "http://localhost:4173",
+    baseUrl: "https://main--vermillion-queijadas-1f4ca3.netlify.app",
   },
   component: {
     specPattern: "src/**/__tests__/*.{cy,spec}.{js,ts,jsx,tsx}",
