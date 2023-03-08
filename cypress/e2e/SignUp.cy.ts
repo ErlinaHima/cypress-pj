11522a59d6a0d468a5b07632c5e1105dbc67f87e
ErlinaHima <e.hima2@gmail.com>
2023-03-08 17:45:16 +0100

    new testcases for Signin and Signout

--- cypress/e2e/SignUp.cy.ts
+++ cypress/e2e/SignUp.cy.ts
@@ -4,21 +4,32 @@ info.email = randomizer.randomizeEmail(info.email);
 
 describe('SignUp Page', () => {
 
-  it('Visit SignUp page', () => {
+  beforeEach('Visit SignUp page', () => {
     cy.visit("/");
     cy.get('#user').should("exist");
     cy.get("a[href='/signup']").click();
     cy.get('#first-name').should("exist");
-  })
+  });
 
   it('SignUp a new user', () => {
-
     cy.get('#first-name').should("exist");
     cy.get('#first-name').type(info.firstName);
     cy.get('#last-name').type(info.lastName);
     cy.get('#email').type(info.email);
     cy.get('#password').type(info.password);
-    cy.get('.btn').should("be.enabled").click()
-    cy.get('.text-success').should("exist")
-  })
-})
+    cy.get('.btn').should("be.enabled").click();
+    cy.get('.home-view').should("be.visible");
+  });
+
+  it('Wrong SignUp a new user', () => {
+    cy.get('#first-name').should("exist");
+    cy.get('#first-name').type(info.firstName);
+    cy.get('#last-name').type(info.lastName);
+    cy.get('#email').type(info.email);// todo add an already register email
+    cy.get('#password').type('test');
+    cy.get('#password').should('have.length.at.least',10).and('match',/\+d/); // password should have at least 10 char
+    cy.get('.btn').should("be.enabled").click();
+    cy.get('.text-success').should("exist");
+  });
+
+});
